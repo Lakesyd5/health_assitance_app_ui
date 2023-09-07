a80c7e6623284ba5313401e8323e9159471646db
Adeleke <adeleoje@gmail.com>
2023-09-07 15:07:49 +0100

    onboarding update

--- lib/commons/utils/app_colors.dart
+++ lib/commons/utils/app_colors.dart
@@ -1,5 +1,16 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 class AppColors {
+  // Background color
   static const Color primaryBackground = Color.fromARGB(255, 65, 84, 88);
+  // specific purple
+  static const Color purple = Color.fromARGB(255, 195, 190, 226);
+  // deep green 
+  static const Color primaryGreen = Color.fromARGB(255, 77, 147, 109);
+  // light green
+  static const Color secondaryGreen = Color.fromARGB(255, 140, 229, 21);
+  // basic white
+  static const Color basicWhite = Colors.white;
+  // primaryText
+  static const Color primaryText = Color.fromARGB(255, 247,247,248);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:health_assitance_ui/features/onboarding/view/welcome.dart';
+import 'package:health_assitance_ui/features/onboarding/view/onboarding.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Welcome(),
+      home: Onboarding(),
       debugShowCheckedModeBanner: false,
     );
   }
